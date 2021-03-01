6c88c501873f6fd113b36e08f4a444829a501d03
Sam Mattiussi <sammattiussi@gmail.com>
2021-03-01 16:42:10 +0100

    Initial Interface for actions

--- octoprint_physicalbutton/__init__.py
+++ octoprint_physicalbutton/__init__.py
@@ -3,50 +3,40 @@ from __future__ import absolute_import
 
 import octoprint.plugin
 
-class PhysicalbuttonPlugin(octoprint.plugin.SettingsPlugin,
-                           octoprint.plugin.AssetPlugin,
+class PhysicalbuttonPlugin(octoprint.plugin.StartupPlugin,
+                           octoprint.plugin.SettingsPlugin,
                            octoprint.plugin.TemplatePlugin):
+    def on_after_startup(self):
+        self._logger.info("Physical Button plugin started")
 
-	##~~ SettingsPlugin mixin
-	def get_settings_defaults(self):
-		return dict(
-			# put your plugin's default settings here
-		)
-
-	##~~ AssetPlugin mixin
-	#def get_assets(self):
-		# Define your plugin's asset files to automatically include in the
-		# core UI here.
-	#	return dict(
-	#		js=["js/physicalbutton.js"],
-	#		css=["css/physicalbutton.css"],
-	#		less=["less/physicalbutton.less"]
-	#	)
 
 	##~~ Softwareupdate hook
-	def get_update_information(self):
-		# Define the configuration for your plugin to use with the Software Update
-		# Plugin here. See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
-		# for details.
-		return dict(
-			physicalbutton=dict(
-				displayName="Physicalbutton Plugin",
-				displayVersion=self._plugin_version,
-
-				# version check: github repository
-				type="github_release",
-				user="LuxuSam",
-				repo="PhysicalButton",
-				current=self._plugin_version,
-
-				# update method: pip
-				pip="https://github.com/LuxuSam/PhysicalButton/archive/{target_version}.zip"
-			)
-		)
+    def get_update_information(self):
+        return dict(
+        physicalbutton = dict(
+        displayName = "Physical Button",
+        displayVersion = self._plugin_version,
+        type = "github_release",
+        user = "LuxuSam",
+        repo = "PhysicalButton",
+        current = self._plugin_version,
+        pip = "https://github.com/LuxuSam/PhysicalButton/archive/{target_version}.zip"
+        )
+        )
 
 
-__plugin_name__ = "Physicalbutton Plugin"
+    def get_template_configs(self):
+        return [
+            dict(type="settings", custom_bindings=False)
+        ]
 
+    def get_template_vars(self):
+        return dict(buttonname=self._settings.get(["buttonname"]))
+
+    
+
+
+__plugin_name__ = "Physical Button"
 __plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
 
 def __plugin_load__():
