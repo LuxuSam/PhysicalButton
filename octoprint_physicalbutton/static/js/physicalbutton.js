23ef211c1dc584466ca54da23a8ce48cde00881d
Sam Mattiussi <sammattiussi@gmail.com>
2021-03-04 21:46:58 +0100

    saving gcode or action button works now

--- octoprint_physicalbutton/static/js/physicalbutton.js
+++ octoprint_physicalbutton/static/js/physicalbutton.js
@@ -12,45 +12,69 @@ $(function() {
         self.settingsViewModel = parameters[0];
 
         //New Button
-        self.newButtonName = ko.observable();
-        self.newButtonGPIO = ko.observable();
-        self.checkedButton = ko.observable();
+        self.newButtonName   = ko.observable();
+        self.newButtonGPIO   = ko.observable();
+        self.checkedButton   = ko.observable();
         self.newButtonAction = ko.observable();
-        self.newButtonGcode = ko.observable();
+        self.newButtonGcode  = ko.observable();
 
         //Saved Buttons
         self.buttons = ko.observable();
+        self.show = ko.observable();
 
+
+        /*
         self.onBeforeBinding = function() {
+            if (self.settingsViewModel.settings.plugins.physicalbutton.buttons() == null){
+                return
+            }
             self.buttons(self.settingsViewModel.settings.plugins.physicalbutton.buttons());
         };
 
         self.onEventSettingsUpdated = function (payload) {
+            if (self.settingsViewModel.settings.plugins.physicalbutton.buttons() == null){
+                return
+            }
             self.buttons(self.settingsViewModel.settings.plugins.physicalbutton.buttons());
         }
-
+        */
         self.addButton = function(){
             if (self.newButtonName == null){
-                console.log("No Name");
+                log.error("No Name for new button");
                 return;
             }
-            if (self.checkedButton() == "checkedGcode"){
-                console.log("Not implemented yet");
-                return;
-            }
-            if (self.checkedButton() == "checkedAction"){
-                console.log("Added Action Button");
-                self.buttons(self.settingsViewModel.settings.plugins.physicalbutton.buttons.push(
-                    {action: self.newButtonAction, buttonname:self.newButtonName, gpio: self.newButtonGPIO}));
+
+            if (self.settingsViewModel.settings.plugins.physicalbutton.buttons() == null){
+                self.settingsViewModel.settings.plugins.physicalbutton.buttons(new Array());
                 self.settingsViewModel.saveData();
-                ;
             }
+
+            if (self.checkedButton() == "checkedGcode"){
+                log.info("Added new GCODE button");
+
+                self.settingsViewModel.settings.plugins.physicalbutton.buttons.push(
+                    {buttonname:self.newButtonName, gpio: self.newButtonGPIO, action: ko.observable(null), gcode: self.newButtonGcode, id: ko.observable(Date.now()), show: ko.observable('gcode')});
+                self.settingsViewModel.saveData();
+            }
+
+            if (self.checkedButton() == "checkedAction"){
+                log.info("Added new Action button");
+
+                self.settingsViewModel.settings.plugins.physicalbutton.buttons.push(
+                    {buttonname:self.newButtonName, gpio: self.newButtonGPIO, action: self.newButtonAction, gcode: ko.observable(null), id: ko.observable(Date.now()), show: ko.observable('action')});
+                self.settingsViewModel.saveData();
+            }
+
+            self.newButtonName(null);
+            self.newButtonGPIO(0);
+            self.checkedButton(null);
+            self.newButtonAction('none');
+            self.newButtonGcode(null);
         };
 
         self.removeButton = function(){
-            self.buttons(self.settingsViewModel.settings.plugins.physicalbutton.buttons.remove(this));
-            //self.settingsViewModel.saveData();
-
+            self.settingsViewModel.settings.plugins.physicalbutton.buttons.remove(this);
+            self.settingsViewModel.saveData();
         };
     }
 
