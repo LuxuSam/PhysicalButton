dca5d4871a93362f743447d30fff95ffcd4d6ede
Sam Mattiussi <sammattiussi@gmail.com>
2021-03-14 16:32:22 +0100

    Fixed wait -> sleep

--- octoprint_physicalbutton/__init__.py
+++ octoprint_physicalbutton/__init__.py
@@ -124,7 +124,7 @@ class PhysicalbuttonPlugin(octoprint.plugin.StartupPlugin,
                     #send commandList to printer
                     self.sendGcode(commandList)
                 #Give user time to release button again
-                time.wait(0.75)
+                time.sleep(0.75)
 
     def sendGcode(self, gcodeCommand):
         self._printer.commands(gcodeCommand, force = False)
