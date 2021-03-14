6e60d43d2c5efccdf9279b981b4920a2c94caaef
Sam Mattiussi <sammattiussi@gmail.com>
2021-03-14 14:10:01 +0100

    Added other test text

--- octoprint_physicalbutton/__init__.py
+++ octoprint_physicalbutton/__init__.py
@@ -96,7 +96,8 @@ class PhysicalbuttonPlugin(octoprint.plugin.StartupPlugin,
         buttonState = GPIO.input(channel)
         bounceTime = int(button.get("buttonTime"))
         while (time.time()*1000 < timePressedButton*1000 + bounceTime):
-            self._logger.info("Button input: %s" %GPIO.input(channel))
+            if buttonState != GPIO.input(channel)
+                self._logger.info("released button")
 
         if (buttonState != GPIO.input(channel)):
             return
