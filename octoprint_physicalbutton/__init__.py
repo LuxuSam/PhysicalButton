31d52e72583481871ab1783a1d19864e98c49113
Sam Mattiussi <sammattiussi@gmail.com>
2021-04-29 17:29:51 +0200

    fixed weird indentation

--- octoprint_physicalbutton/__init__.py
+++ octoprint_physicalbutton/__init__.py
@@ -237,13 +237,13 @@ __plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
 
 
 def __plugin_load__():
-	global __plugin_implementation__
-	__plugin_implementation__ = PhysicalbuttonPlugin()
+    global __plugin_implementation__
+    __plugin_implementation__ = PhysicalbuttonPlugin()
 
-	global __plugin_hooks__
-	__plugin_hooks__ = {
-		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-	}
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+	   "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+    }
 
     global callbackIsRunning
     callbackIsRunning = False
