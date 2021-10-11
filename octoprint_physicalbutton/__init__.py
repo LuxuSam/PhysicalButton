0c1c143a1ab1bb2ffa45cc7bb5123a7d8d9e6088
Sam Mattiussi <72255076+LuxuSam@users.noreply.github.com>
2021-10-11 13:50:58 +0200

    [WIP] Feature output activity (#45)
    * Added interface for output activity
    
    * Added functionality to add an output activity
    
    * added check to prevent setting a pin to output and input
    
    * compact output configuration
    
    * Output pin can now be used for multiple buttons
    
    * added functionality for GPIO output setup
    
    * call removeOutputs() for shutdown, save and cleanup
    
    * started implementation for output-activity reaction
    
    * added functionality to generate output
    
    * removed async variable to test directly in if condition
    
    * convert filter object to list
    
    * print outputList for debugging in generateOutput
    
    * changed list to iter
    
    * catch case where no GPIO is specified
    
    * added interface to set initial value for GPIO
    
    * equate initial values if necessary
    
    * Added tooltip for initial value
    
    * set initial value for output devices in setup

--- octoprint_physicalbutton/__init__.py
+++ octoprint_physicalbutton/__init__.py
@@ -2,12 +2,13 @@
 from __future__ import absolute_import
 
 import octoprint.plugin
-from gpiozero import Button
+from gpiozero import Button,OutputDevice
 import time
 import threading
 import subprocess
 
 buttonList = []
+outputList = []
 
 class PhysicalbuttonPlugin(octoprint.plugin.StartupPlugin,
                            octoprint.plugin.SettingsPlugin,
@@ -30,7 +31,22 @@ class PhysicalbuttonPlugin(octoprint.plugin.StartupPlugin,
             if buttonMode == "Normally Closed (NC)":
                 newButton.when_released = self.reactToInput
             buttonList.append(newButton)
+            self.setupOutputPins(button)
         self._logger.debug('Added Buttons: %s' %buttonList)
+        self._logger.debug('Added Output devices: %s' %outputList)
+
+    def setupOutputPins(self,button):
+        global outputList
+        for activity in list(filter(lambda a: a.get("type") == "output", button.get("activities"))):
+            outputGPIO = activity.get("execute").get("gpio")
+            #check if gpio has to be setup
+            if outputGPIO == 'none' or int(outputGPIO) in list(map(lambda oD: oD.pin.number, outputList)):
+                continue
+            outputDevice = OutputDevice(int(outputGPIO))
+            initialValue = activity.get("execute").get("initial")
+            if initialValue == "HIGH":
+                outputDevice.on()
+            outputList.append(outputDevice)
 
     def removeButtons(self):
         global buttonList
@@ -38,6 +54,13 @@ class PhysicalbuttonPlugin(octoprint.plugin.StartupPlugin,
         for button in buttonList:
             button.close()
         buttonList.clear()
+
+    def removeOutputs(self):
+        global outputList
+        self._logger.debug('Output devices to remove: %s' %outputList)
+        for outputDevice in outputList:
+            outputDevice.close()
+        outputList.clear()
 
     def thread_react(self, pressedButton):
         #save value of button (pushed or released)
@@ -72,12 +95,19 @@ class PhysicalbuttonPlugin(octoprint.plugin.StartupPlugin,
                 if activity.get("type") == "file":
                     #select the file at the given location
                     exitCode = self.selectFile(activity.get("execute"))
+                if activity.get("type") == "output":
+                    #generate output for given amount of time
+                    exitCode = self.generateOutput(activity.get("execute"))
                 #Check if an executed activity failed
                 if exitCode == 0:
                     self._logger.debug("The activity with identifier '%s' was executed successfully!" %activity.get("identifier"))
+                    continue
                 if exitCode == -1:
                     self._logger.error("The activity with identifier '%s' failed! Aborting follwing activities!" %activity.get("identifier") )
                     break
+                if exitCode == -2:
+                    self._logger.error("The activity with identifier '%s' failed! No GPIO specified!" %activity.get("identifier"))
+                    continue
 
     def reactToInput(self, pressedButton):
         t = threading.Thread(target=self.thread_react, args=(pressedButton,))
@@ -163,6 +193,42 @@ class PhysicalbuttonPlugin(octoprint.plugin.StartupPlugin,
             self._logger.error(e)
             return -1
 
+    def generateOutput(self, output):
+        global outputList
+
+        if output.get("gpio") == 'none':
+            return -2
+
+        gpio = int(output.get("gpio"))
+        value = output.get("value")
+        time = int(output.get("time"))
+
+        outputDevice = next(iter(filter(lambda oD: oD.pin.number == gpio, outputList)))
+
+        if output.get("async") == 'True':
+            t = threading.Thread(target = self.setOutput, args=(value, time, outputDevice,))
+            t.start()
+        else:
+            self.setOutput(value, time, outputDevice)
+        return 0
+
+    def setOutput(self, value, activeTime, outputDevice):
+        if value == 'HIGH':
+            outputDevice.on()
+        elif value == 'LOW':
+            outputDevice.off()
+        elif value == 'Toggle':
+            outputDevice.toggle()
+
+        if activeTime == 0:
+            return
+        else:
+            time.sleep(activeTime/1000)
+
+        outputDevice.toggle()
+
+
+
     ####################################_Custom actions_##############################################
     def toggle_cancel_print(self):
         if self._printer.is_ready():
@@ -185,12 +251,14 @@ class PhysicalbuttonPlugin(octoprint.plugin.StartupPlugin,
             return
         self._logger.info("Cleaning up used GPIOs before shutting down ...")
         self.removeButtons()
+        self.removeOutputs()
         self._logger.info("Done!")
 
     def on_settings_save(self, data):
         #Handle old configuration:
         if self._settings.get(["buttons"]) != None and self._settings.get(["buttons"]) != []:
             self.removeButtons()
+            self.removeOutputs()
             self._logger.debug("Removed old button configuration")
         #Save new Settings
         octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
@@ -201,6 +269,7 @@ class PhysicalbuttonPlugin(octoprint.plugin.StartupPlugin,
 
     def on_settings_cleanup(self):
         self.removeButtons()
+        self.removeOutputs()
         octoprint.plugin.SettingsPlugin.on_settings_cleanup(self)
 
     def get_settings_defaults(self):
--- octoprint_physicalbutton/static/js/physicalbutton.js
+++ octoprint_physicalbutton/static/js/physicalbutton.js
@@ -4,178 +4,247 @@
  * Author: Sam
  * License: AGPLv3
  */
- $(function() {
-     function PhysicalbuttonViewModel(parameters) {
-         var self = this;
+$(function() {
+    function PhysicalbuttonViewModel(parameters) {
+        var self = this;
 
-         //settings
-         self.settingsViewModel = parameters[0];
+        //settings
+        self.settingsViewModel = parameters[0];
 
-         //GPIOs:
-         self.gpios = ko.observableArray(['none', '2', '3', '4', '5', '6',
-             '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17',
-             '18', '19', '20', '21', '22', '23', '24', '25', '26', '27'
-         ]);
-         //actions:
-         self.actions = ko.observableArray(['none', 'connect', 'disconnect', 'home', 'pause', 'resume', 'toggle pause-resume', 'start', 'cancel', 'toggle start-cancel']);
+        //GPIOs:
+        self.gpios = ko.observableArray(['none', '2', '3', '4', '5', '6',
+            '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17',
+            '18', '19', '20', '21', '22', '23', '24', '25', '26', '27'
+        ]);
+        //actions:
+        self.actions = ko.observableArray(['none', 'connect', 'disconnect', 'home', 'pause', 'resume', 'toggle pause-resume', 'start', 'cancel', 'toggle start-cancel']);
 
-         //button modes:
-         self.buttonModes = ko.observableArray(['Normally Open (NO)', 'Normally Closed (NC)']);
+        //button modes:
+        self.buttonModes = ko.observableArray(['Normally Open (NO)', 'Normally Closed (NC)']);
 
-         //Saved Buttons
-         self.buttons = ko.observableArray();
+        //Saved Buttons
+        self.buttons = ko.observableArray();
 
-         self.selectedGPIO = ko.observable();
+        self.selectedGPIO = ko.observable();
 
-         self.selectedActivity = ko.observable();
-         self.index = ko.observable(1);
+        self.selectedActivity = ko.observable();
+        self.index = ko.observable(1);
 
-         self.changeDetected = ko.observable(false);
+        self.changeDetected = ko.observable(false);
 
-         self.onBeforeBinding = function() {
-             self.buttons(self.settingsViewModel.settings.plugins.physicalbutton.buttons());
-         };
+        self.onBeforeBinding = function() {
+            self.buttons(self.settingsViewModel.settings.plugins.physicalbutton.buttons());
+        };
 
-         self.onSettingsBeforeSave = function() {
-             self.settingsViewModel.settings.plugins.physicalbutton.buttons(self.buttons());
-             self.changeDetected(false);
-         };
+        self.onSettingsBeforeSave = function() {
+            self.settingsViewModel.settings.plugins.physicalbutton.buttons(self.buttons());
+            self.changeDetected(false);
+        };
 
-         self.onSettingsShown = function() {
-             self.buttons(self.settingsViewModel.settings.plugins.physicalbutton.buttons());
-         };
+        self.onSettingsShown = function() {
+            self.buttons(self.settingsViewModel.settings.plugins.physicalbutton.buttons());
+        };
 
-         self.viewChanged = function(obj, event) {
+        self.viewChanged = function(obj, event) {
             if (event.originalEvent) { //user changed
                 self.changeDetected(true);
             }
         }
 
-         self.disableGpioOption = function(item, currentGPIO) {
-             if (item == 'none') {
-                 return false;
-             }
-             if (self.buttons().find(b => b.gpio() == item)) {
-                 if (item != currentGPIO()) {
-                     return true;
-                 }
-             }
-             return false;
-         };
+        self.disableGpioOption = function(item, currentGPIO) {
+            if (item == 'none' || item == currentGPIO())
+                return false;
 
-         self.addButton = function() {
-             if (!self.buttons()) {
-                 self.buttons(new Array());
-             }
-             self.buttons.push({
-                 activities: ko.observableArray(new Array()),
-                 buttonMode: ko.observable('Normally Open (NO)'),
-                 buttonName: ko.observable('New Button Name'),
-                 gpio: ko.observable('none'),
-                 buttonTime: ko.observable('500'),
-                 id: ko.observable(Date.now())
-             });
-         };
+            //disable used input gpios
+            if (self.buttons().find(b => b.gpio() == item))
+                return true;
 
-         self.removeButton = function() {
-             self.buttons.remove(this)
-         };
+            //disable used output gpios
+            if (self.buttons().find(b => b.activities().find(a => a.type() == 'output' && a.execute.gpio() == item)))
+                return true;
 
-         self.addAction = function() {
-             var updatedItem = this;
-             if (!updatedItem.activities()) {
-                 updatedItem.activities(new Array);
-             }
-             updatedItem.activities.push({
-                 type: ko.observable('action'),
-                 identifier: ko.observable('New Action'),
-                 execute: ko.observable('none')
-             });
-             self.selectedActivity(this.activities()[this.activities().length - 1]);
-         };
+            return false;
+        };
 
-         self.addGCODE = function() {
-             var updatedItem = this;
-             if (!updatedItem.activities()) {
-                 updatedItem.activities(new Array);
-             }
-             updatedItem.activities.push({
-                 type: ko.observable('gcode'),
-                 identifier: ko.observable('New GCODE'),
-                 execute: ko.observable('')
-             });
-             self.selectedActivity(this.activities()[this.activities().length - 1]);
-         };
+        self.disableGpioOutputOption = function(item){
+            if (item == 'none')
+                return false;
 
-         self.addSystem = function() {
-             var updatedItem = this;
-             if (!updatedItem.activities()) {
-                 updatedItem.activities(new Array);
-             }
-             updatedItem.activities.push({
-                 type: ko.observable('system'),
-                 identifier: ko.observable('New System Command'),
-                 execute: ko.observable('')
-             });
-             self.selectedActivity(this.activities()[this.activities().length - 1]);
-         };
+            //disable used input gpios
+            if (self.buttons().find(b => b.gpio() == item))
+                return true;
+        }
 
-         self.addFile = function() {
-             var updatedItem = this;
-             if (!updatedItem.activities()) {
-                 updatedItem.activities(new Array);
-             }
-             updatedItem.activities.push({
-                 type: ko.observable('file'),
-                 identifier: ko.observable('New File'),
-                 execute: ko.observable('')
-             });
-             self.selectedActivity(this.activities()[this.activities().length - 1]);
-         };
+        self.addButton = function() {
+            if (!self.buttons()) {
+                self.buttons(new Array());
+            }
+            self.buttons.push({
+                activities: ko.observableArray(new Array()),
+                buttonMode: ko.observable('Normally Open (NO)'),
+                buttonName: ko.observable('New Button Name'),
+                gpio: ko.observable('none'),
+                buttonTime: ko.observable('500'),
+                id: ko.observable(Date.now())
+            });
+        };
 
-         self.removeActivity = function() {
-             if (!self.selectedActivity()) {
-                 return;
-             }
-             this.activities.remove(self.selectedActivity());
-             self.selectedActivity(this.activities()[this.activities().length - 1]);
-         };
+        self.removeButton = function() {
+            self.buttons.remove(this)
+        };
 
-         self.changeSelection = function() {
-             self.selectedActivity(this.activities()[0]);
-         };
+        self.addAction = function() {
+            var updatedItem = this;
+            if (!updatedItem.activities()) {
+                updatedItem.activities(new Array);
+            }
+            updatedItem.activities.push({
+                type: ko.observable('action'),
+                identifier: ko.observable('New Action'),
+                execute: ko.observable('none')
+            });
+            self.selectedActivity(this.activities()[this.activities().length - 1]);
+        };
 
-         self.updatePosition = function() {
-             const length = this.activities().length
-             if (length < 2)
-                 return;
-             if (self.index() < 1) {
-                 self.index(1)
-             }
-             if (self.index() > length) {
-                 self.index(length)
-             }
-             const current = self.selectedActivity();
-             const amount = length - self.index();
-             this.activities.remove(current);
-             const spliced = this.activities.splice(self.index() - 1, amount);
-             this.activities.push(current);
-             for (var i = 0; i < spliced.length; i++) {
-                 this.activities.push(spliced[i]);
-             }
-             self.index(1)
-         };
-     }
+        self.addGCODE = function() {
+            var updatedItem = this;
+            if (!updatedItem.activities()) {
+                updatedItem.activities(new Array);
+            }
+            updatedItem.activities.push({
+                type: ko.observable('gcode'),
+                identifier: ko.observable('New GCODE'),
+                execute: ko.observable('')
+            });
+            self.selectedActivity(this.activities()[this.activities().length - 1]);
+        };
 
-     /* view model class, parameters for constructor, container to bind to
-      * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
-      * and a full list of the available options.
-      */
-     OCTOPRINT_VIEWMODELS.push({
-         construct: PhysicalbuttonViewModel,
-         // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
-         dependencies: ["settingsViewModel"],
-         // Elements to bind to, e.g. #settings_plugin_physicalbutton, #tab_plugin_physicalbutton, ...
-         elements: ["#settings_plugin_physicalbutton"]
-     });
- });
+        self.addSystem = function() {
+            var updatedItem = this;
+            if (!updatedItem.activities()) {
+                updatedItem.activities(new Array);
+            }
+            updatedItem.activities.push({
+                type: ko.observable('system'),
+                identifier: ko.observable('New System Command'),
+                execute: ko.observable('')
+            });
+            self.selectedActivity(this.activities()[this.activities().length - 1]);
+        };
+
+        self.addFile = function() {
+            var updatedItem = this;
+            if (!updatedItem.activities()) {
+                updatedItem.activities(new Array);
+            }
+            updatedItem.activities.push({
+                type: ko.observable('file'),
+                identifier: ko.observable('New File'),
+                execute: ko.observable('')
+            });
+            self.selectedActivity(this.activities()[this.activities().length - 1]);
+        };
+
+        self.addOutput = function() {
+            var updatedItem = this;
+            if (!updatedItem.activities()) {
+                updatedItem.activities(new Array);
+            }
+            updatedItem.activities.push({
+                type: ko.observable('output'),
+                identifier: ko.observable('New Output'),
+                execute: {
+                    gpio: ko.observable('none'),
+                    value: ko.observable('HIGH'),
+                    time: ko.observable('500'),
+                    async: ko.observable('False'),
+                    initial: ko.observable('LOW'),
+                    id: ko.observable(Date.now())
+                }
+            });
+            self.selectedActivity(this.activities()[this.activities().length - 1]);
+        }
+
+        self.initialValueChanged = function(initialValue, gpio, id, data, event){
+            if (gpio == 'none'){
+                return;
+            }
+            console.log(event.originalEvent);
+            if (event.originalEvent) { //user changed
+                if (initialValue == 'HIGH'){ //toggle value as old initial value is passed
+                    initialValue = 'LOW'
+                }else{
+                    initialValue = 'HIGH'
+                }
+                for (let button of self.buttons()){ //set all initial values for this gpio to the new initial value
+                    for (let activity of button.activities()){
+                        if (activity.type() == 'output' && activity.execute.id() != id && activity.execute.gpio() != 'none' && activity.execute.gpio() == gpio){
+                            activity.execute.initial(initialValue);
+                        }
+                    }
+                }
+            }
+        }
+
+        self.outputGpioChanged = function(gpio, id, data, event){
+            if (gpio == 'none'){
+                return;
+            }
+            console.log(event.originalEvent);
+            if (event.originalEvent) {
+                for (let button of self.buttons()){ //set all initial values for this gpio to the new initial value
+                    const activity = button.activities().find(activity => activity.type() == 'output' && activity.execute.id() != id && activity.execute.gpio() != 'none' && activity.execute.gpio() == gpio)
+                    if (activity){
+                        self.selectedActivity().execute.initial(activity.execute.initial());
+                        return;
+                    }
+                }
+            }
+        }
+
+        self.removeActivity = function() {
+            if (!self.selectedActivity()) {
+                return;
+            }
+            this.activities.remove(self.selectedActivity());
+            self.selectedActivity(this.activities()[this.activities().length - 1]);
+        };
+
+        self.changeSelection = function() {
+            self.selectedActivity(this.activities()[0]);
+        };
+
+        self.updatePosition = function() {
+            const length = this.activities().length
+            if (length < 2)
+                return;
+            if (self.index() < 1) {
+                self.index(1)
+            }
+            if (self.index() > length) {
+                self.index(length)
+            }
+            const current = self.selectedActivity();
+            const amount = length - self.index();
+            this.activities.remove(current);
+            const spliced = this.activities.splice(self.index() - 1, amount);
+            this.activities.push(current);
+            for (var i = 0; i < spliced.length; i++) {
+                this.activities.push(spliced[i]);
+            }
+            self.index(1)
+        };
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: PhysicalbuttonViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: ["settingsViewModel"],
+        // Elements to bind to, e.g. #settings_plugin_physicalbutton, #tab_plugin_physicalbutton, ...
+        elements: ["#settings_plugin_physicalbutton"]
+    });
+});
