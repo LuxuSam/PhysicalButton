7702562c560ede6c0ce5912f78350c59205b0b01
Sam Mattiussi <72255076+LuxuSam@users.noreply.github.com>
2021-10-11 18:30:50 +0200

    f-String & Code organization (#48)
    * changed all %-formatting to f-formatting
    
    * change .get("*") to .get('*')
    
    * Catch unknown activities
    
    * organize order of functions

--- octoprint_physicalbutton/__init__.py
+++ octoprint_physicalbutton/__init__.py
@@ -20,15 +20,17 @@ class PhysicalbuttonPlugin(octoprint.plugin.AssetPlugin,
                            octoprint.plugin.StartupPlugin,
                            octoprint.plugin.TemplatePlugin
                            ):
-
-    ########################################_Helper functions_########################################
+                           
+    ##################################################################################################
+    ########################################_GPIO Setup functions_####################################
+    ##################################################################################################
     def setupButtons(self):
         global buttonList
         for button in self._settings.get(["buttons"]):
-            if button.get("gpio") == "none":
+            if button.get('gpio') == "none":
                 continue
-            buttonGPIO = int(button.get("gpio"))
-            buttonMode = button.get("buttonMode")
+            buttonGPIO = int(button.get('gpio'))
+            buttonMode = button.get('buttonMode')
             newButton = Button(buttonGPIO, pull_up=True, bounce_time=None)
             if buttonMode == "Normally Open (NO)":
                 newButton.when_pressed = self.reactToInput
@@ -36,90 +38,99 @@ class PhysicalbuttonPlugin(octoprint.plugin.AssetPlugin,
                 newButton.when_released = self.reactToInput
             buttonList.append(newButton)
             self.setupOutputPins(button)
-        self._logger.debug('Added Buttons: %s' %buttonList)
-        self._logger.debug('Added Output devices: %s' %outputList)
+        self._logger.debug(f"Added Buttons: {buttonList}")
+        self._logger.debug(f"Added Output devices: {outputList}")
 
     def setupOutputPins(self,button):
         global outputList
-        for activity in list(filter(lambda a: a.get("type") == "output", button.get("activities"))):
-            outputGPIO = activity.get("execute").get("gpio")
+        for activity in list(filter(lambda a: a.get('type') == "output", button.get('activities'))):
+            outputGPIO = activity.get('execute').get('gpio')
             #check if gpio has to be setup
             if outputGPIO == 'none' or int(outputGPIO) in list(map(lambda oD: oD.pin.number, outputList)):
                 continue
             outputDevice = OutputDevice(int(outputGPIO))
-            initialValue = activity.get("execute").get("initial")
+            initialValue = activity.get('execute').get('initial')
             if initialValue == "HIGH":
                 outputDevice.on()
             outputList.append(outputDevice)
 
     def removeButtons(self):
         global buttonList
-        self._logger.debug('Buttons to remove: %s' %buttonList)
+        self._logger.debug(f"Buttons to remove: {buttonList}")
         for button in buttonList:
             button.close()
         buttonList.clear()
 
     def removeOutputs(self):
         global outputList
-        self._logger.debug('Output devices to remove: %s' %outputList)
+        self._logger.debug(f"Output devices to remove: {outputList}")
         for outputDevice in outputList:
             outputDevice.close()
         outputList.clear()
 
+    ##################################################################################################
+    ########################################_React to button_#########################################
+    ##################################################################################################
     def thread_react(self, pressedButton):
         #save value of button (pushed or released)
         buttonValue = pressedButton.value
 
         #search for pressed button
         for btn in self._settings.get(["buttons"]):
-            if btn.get("gpio") == "none":
+            if btn.get('gpio') == "none":
                 continue
-            if int(btn.get("gpio")) == pressedButton.pin.number:
+            if int(btn.get('gpio')) == pressedButton.pin.number:
                 button = btn
                 break
 
-        waitTime = int(button.get("buttonTime"))
+        waitTime = int(button.get('buttonTime'))
         time.sleep(waitTime/1000)
 
         if pressedButton.value == buttonValue:
-            self._logger.debug("Reacting to button %s:" %button.get("buttonName"))
+            self._logger.debug(f"Reacting to button {button.get('buttonName')}")
             #execute actions for button in order
-            for activity in button.get("activities"):
+            for activity in button.get('activities'):
                 exitCode = 0
-                self._logger.debug("Sending activity with identifier '%s' ..." %activity.get("identifier"))
-                if activity.get("type") == "action":
+                self._logger.debug(f"Sending activity with identifier '{activity.get('identifier')}' ...")
+                if activity.get('type') == "action":
                     #send specified action
-                    exitCode = self.sendAction(activity.get("execute"))
-                if activity.get("type") == "gcode":
+                    exitCode = self.sendAction(activity.get('execute'))
+                elif activity.get('type') == "gcode":
                     #send specified gcode
-                    exitCode = self.sendGcode(activity.get("execute"))
-                if activity.get("type") == "system":
+                    exitCode = self.sendGcode(activity.get('execute'))
+                elif activity.get('type') == "system":
                     #send specified system
-                    exitCode = self.runSystem(activity.get("execute"))
-                if activity.get("type") == "file":
+                    exitCode = self.runSystem(activity.get('execute'))
+                elif activity.get('type') == "file":
                     #select the file at the given location
-                    exitCode = self.selectFile(activity.get("execute"))
-                if activity.get("type") == "output":
+                    exitCode = self.selectFile(activity.get('execute'))
+                elif activity.get('type') == "output":
                     #generate output for given amount of time
-                    exitCode = self.generateOutput(activity.get("execute"))
+                    exitCode = self.generateOutput(activity.get('execute'))
+                else:
+                    self._logger.debug(f"The activity with identifier '{activity.get('identifier')}' is not known (yet)!")
+                    continue
                 #Check if an executed activity failed
                 if exitCode == 0:
-                    self._logger.debug("The activity with identifier '%s' was executed successfully!" %activity.get("identifier"))
+                    self._logger.debug(f"The activity with identifier '{activity.get('identifier')}' was executed successfully!")
                     continue
                 if exitCode == -1:
-                    self._logger.error("The activity with identifier '%s' failed! Aborting follwing activities!" %activity.get("identifier") )
+                    self._logger.error(f"The activity with identifier '{activity.get('identifier')}' failed! Aborting follwing activities!")
                     break
                 if exitCode == -2:
-                    self._logger.error("The activity with identifier '%s' failed! No GPIO specified!" %activity.get("identifier"))
+                    self._logger.error(f"The activity with identifier '{activity.get('identifier')}' failed! No GPIO specified!")
                     continue
 
     def reactToInput(self, pressedButton):
         t = threading.Thread(target=self.thread_react, args=(pressedButton,))
         t.start()
 
+    ##################################################################################################
+    ########################################_Activities_##############################################
+    ##################################################################################################
     def sendGcode(self, gcodetxt):
         if not self._printer.is_operational():
-            self._logger.error("Your machine is not operational!")
+            self._logger.error(f"Your machine is not operational!")
             return -1
         #split gcode lines in single commands without comment and add to list
         commandList = []
@@ -159,90 +170,91 @@ class PhysicalbuttonPlugin(octoprint.plugin.AssetPlugin,
         if action == 'toggle start-cancel':
             self.toggle_cancel_print()
             return 0
-        self._logger.debug("No action selected or action (yet) unknown")
+        self._logger.debug(f"No action selected or action (yet) unknown")
         return 0
 
     def runSystem(self, commands):
         # split commands lines and execute one by one, unless there is an error
         for command in commands.splitlines():
-            self._logger.info("Executing system command '%s'" % (command))
+            self._logger.info(f"Executing system command '{command}'")
 
             try:
                 # send command to Pi
                 ret = subprocess.check_output(command,
                     stderr=subprocess.STDOUT, shell=True)
                 # log output
-                self._logger.info("Command '%s' returned: %s" %
-                    (command, ret.decode("utf-8")))
+                self._logger.info(f"Command '{command}' returned: {ret.decode('utf-8')}")
                 return 0
             except subprocess.CalledProcessError as e:
                 # return exception and stop further processing
-                self._logger.error("Error [%d] executing command '%s': %s" %
-                    (e.returncode, command, e.output.decode("utf-8")))
+                self._logger.error(f"Error [{e.returncode}] executing command '{command}': {e.output.decode('utf-8')}")
                 return -1
 
     def selectFile(self, path):
         try:
             if not self._printer.is_ready():
-                self._logger.error("Your machine is not ready to select a file!")
+                self._logger.error(f"Your machine is not ready to select a file!")
                 return -1
             if '@sd:' in path:
                 path = path.replace('@sd:','').strip()
                 self._printer.select_file(path, True, printAfterSelect = False)
-                self._logger.debug("Selecting SD-file '%s'" %path )
+                self._logger.debug(f"Selecting SD-file '{path}'")
             else:
                 path = path.strip()
                 self._printer.select_file(path, False, printAfterSelect = False)
-                self._logger.debug("Selecting file '%s'" %path )
+                self._logger.debug(f"Selecting file '{path}'")
             return 0
         except (octoprint.printer.InvalidFileType, octoprint.printer.InvalidFileLocation) as e:
             self._logger.error(e)
             return -1
 
-    def updateLatestFilePath(self):
-        global latestFilePath
-
-        files = self._file_manager.list_files(recursive = True)
-        localFileDict = self.getLatestPath(files.get("local"), None, -1)
-        pathLocal = localFileDict.get("path")
-        latestFilePath = pathLocal
-
-    def getLatestPath(self, files, latestPath, latestDate):
-        for file in files:
-            file = files.get(file)
-            if file.get("type") == "folder":
-                fileDict = self.getLatestPath(file.get("children"), latestPath, latestDate)
-                latestPath = fileDict.get("path")
-                latestDate = fileDict.get("date")
-
-            if file.get("type") == "machinecode":
-                if file.get("date") > latestDate:
-                    latestPath = file.get("path")
-                    latestDate = file.get("date")
-
-        return {
-            "path" : latestPath,
-            "date" : latestDate
-        }
-
     def generateOutput(self, output):
         global outputList
 
-        if output.get("gpio") == 'none':
+        if output.get('gpio') == 'none':
             return -2
 
-        gpio = int(output.get("gpio"))
-        value = output.get("value")
-        time = int(output.get("time"))
+        gpio = int(output.get('gpio'))
+        value = output.get('value')
+        time = int(output.get('time'))
 
         outputDevice = next(iter(filter(lambda oD: oD.pin.number == gpio, outputList)))
 
-        if output.get("async") == 'True':
+        if output.get('async') == 'True':
             t = threading.Thread(target = self.setOutput, args=(value, time, outputDevice,))
             t.start()
         else:
             self.setOutput(value, time, outputDevice)
         return 0
+
+    ##################################################################################################
+    ########################################_Helper functions_########################################
+    ##################################################################################################
+    def updateLatestFilePath(self):
+        global latestFilePath
+
+        files = self._file_manager.list_files(recursive = True)
+        localFileDict = self.getLatestPath(files.get('local'), None, -1)
+        pathLocal = localFileDict.get('path')
+        latestFilePath = pathLocal
+
+    def getLatestPath(self, files, latestPath, latestDate):
+        for file in files:
+            file = files.get(file)
+            if file.get('type') == "folder":
+                fileDict = self.getLatestPath(file.get('children'), latestPath, latestDate)
+                latestPath = fileDict.get('path')
+                latestDate = fileDict.get('date')
+
+            if file.get('type') == "machinecode":
+                if file.get('date') > latestDate:
+                    latestPath = file.get('path')
+                    latestDate = file.get('date')
+
+        return {
+            "path" : latestPath,
+            "date" : latestDate
+        }
 
     def setOutput(self, value, activeTime, outputDevice):
         if value == 'HIGH':
@@ -259,7 +271,9 @@ class PhysicalbuttonPlugin(octoprint.plugin.AssetPlugin,
 
         outputDevice.toggle()
 
-    ####################################_Custom actions_##############################################
+    ##################################################################################################
+    ########################################_Custom actions_##########################################
+    ##################################################################################################
     def toggle_cancel_print(self):
         if self._printer.is_ready():
             self._printer.start_print()
@@ -268,11 +282,11 @@ class PhysicalbuttonPlugin(octoprint.plugin.AssetPlugin,
 
     def start_latest(self):
         if (latestFilePath is None) or (not self._file_manager.file_exists("local",latestFilePath)):
-            self._logger.debug("latestFilePath not set yet, start search")
+            self._logger.debug(f"latestFilePath not set yet, start search")
             self.updateLatestFilePath()
 
         if latestFilePath is None:
-            self._logger.error('No files found!')
+            self._logger.error(f"No files found!")
             return -1
 
         if self.selectFile(latestFilePath) == -1:
@@ -281,42 +295,44 @@ class PhysicalbuttonPlugin(octoprint.plugin.AssetPlugin,
         self._printer.start_print()
         return 0
 
-    ####################################_OctoPrint Functions_#########################################
+    ##################################################################################################
+    ########################################_OctoPrint Functions_#####################################
+    ##################################################################################################
     def on_event(self, event, payload):
         if event == "FileAdded":
             global latestFilePath
-            latestFilePath = payload.get("path")
-            self._logger.debug("Added new file: %s" %latestFilePath)
+            latestFilePath = payload.get('path')
+            self._logger.debug(f"Added new file: {latestFilePath}")
 
     def on_after_startup(self):
         if self._settings.get(["buttons"]) == None or self._settings.get(["buttons"]) == []:
-            self._logger.debug("No buttons to initialize!")
+            self._logger.debug(f"No buttons to initialize!")
             return
-        self._logger.debug("Setting up buttons ...")
+        self._logger.debug(f"Setting up buttons ...")
         self.setupButtons()
-        self._logger.info("Buttons have been set up!")
+        self._logger.info(f"Buttons have been set up!")
 
     def on_shutdown(self):
         if self._settings.get(["buttons"]) == None or self._settings.get(["buttons"]) == []:
-            self._logger.debug("No buttons to clean up ...")
+            self._logger.debug(f"No buttons to clean up ...")
             return
-        self._logger.info("Cleaning up used GPIOs before shutting down ...")
+        self._logger.info(f"Cleaning up used GPIOs before shutting down ...")
         self.removeButtons()
         self.removeOutputs()
-        self._logger.info("Done!")
+        self._logger.info(f"Done!")
 
     def on_settings_save(self, data):
         #Handle old configuration:
         if self._settings.get(["buttons"]) != None and self._settings.get(["buttons"]) != []:
             self.removeButtons()
             self.removeOutputs()
-            self._logger.debug("Removed old button configuration")
+            self._logger.debug(f"Removed old button configuration")
         #Save new Settings
         octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
         #Handle new configuration
         if self._settings.get(["buttons"]) != None and self._settings.get(["buttons"]) != []:
             self.setupButtons()
-            self._logger.debug("Added new button configuration")
+            self._logger.debug(f"Added new button configuration")
 
     def on_settings_cleanup(self):
         self.removeButtons()
