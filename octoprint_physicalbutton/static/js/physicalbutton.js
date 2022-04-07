f40aeb9107d56a7bc7b4a97bbfb2fc32afd994d1
LuxuSam <sammattiussi@gmail.com>
2022-04-07 15:01:44 +0200

    added one todo as reminder to remove test function

--- octoprint_physicalbutton/static/js/physicalbutton.js
+++ octoprint_physicalbutton/static/js/physicalbutton.js
@@ -62,7 +62,7 @@ $(function() {
 
         };
 
-
+        //TODO: remove occurrences of this when finished
         self.showExternalActions = function() {
             self.supportedPlugins(
                 {
