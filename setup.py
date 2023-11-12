a74c02957500c5803c50bc7b33224c688e151e1a
Sam Mattiussi <sammattiussi@gmail.com>
2023-11-12 16:00:27 +0100

    specify gpiozero version to use

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/LuxuSam/PhysicalButton"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ['gpiozero']
+plugin_requires = ['gpiozero==1.6.2']
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
